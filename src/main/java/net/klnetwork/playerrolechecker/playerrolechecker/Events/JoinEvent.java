e3dc9431838a2b085459c7d8148eee116043536c
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-08-31 18:29:54 +0900

    fix error

--- src/main/java/net/klnetwork/playerrolechecker/playerrolechecker/Events/JoinEvent.java
+++ src/main/java/net/klnetwork/playerrolechecker/playerrolechecker/Events/JoinEvent.java
@@ -1,6 +1,7 @@
 package net.klnetwork.playerrolechecker.playerrolechecker.Events;
 
 import net.klnetwork.playerrolechecker.playerrolechecker.API.CodeUtil;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
@@ -10,6 +11,6 @@ import java.util.UUID;
 public class JoinEvent implements Listener {
     @EventHandler
     public void CodeIssue(AsyncPlayerPreLoginEvent e) {
-        UUID uuid = e.getUniqueId();
+        e.disallow(AsyncPlayerPreLoginEvent.Result.KICK_WHITELIST, ChatColor.RED + "あなたのコードは \n「" + CodeUtil.CodeIssue(e.getUniqueId()) + "」 です");
     }
 }
--- src/main/java/net/klnetwork/playerrolechecker/playerrolechecker/MySQL/SQLite.java
+++ src/main/java/net/klnetwork/playerrolechecker/playerrolechecker/MySQL/SQLite.java
@@ -6,35 +6,17 @@ import static net.klnetwork.playerrolechecker.playerrolechecker.PlayerRoleChecke
 
 public class SQLite {
     public static boolean CheckCode(Integer code) {
-        Connection connection = null;
-        Statement statement = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
         try {
-            connection = DriverManager.getConnection("jdbc:sqlite:" + SQLLocate);
-            statement = connection.createStatement();
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:" + SQLLocate);
+            Statement statement = connection.createStatement();
             statement.setQueryTimeout(30);
-            preparedStatement = connection.prepareStatement("select * from waitverify where code = ?" );
-            preparedStatement.setString(1,code.toString());
-            resultSet = preparedStatement.executeQuery();
+            PreparedStatement preparedStatement = connection.prepareStatement("select * from waitverify where code = ?");
+            preparedStatement.setString(1, code.toString());
+            ResultSet resultSet = preparedStatement.executeQuery();
             return resultSet.next();
         } catch (SQLException throwables) {
             throwables.printStackTrace();
-        }finally {
-            if(connection != null && statement != null && preparedStatement != null && resultSet != null){
-                try {
-                    connection.close();
-                    statement.close();
-                    preparedStatement.close();
-                    resultSet.close();
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-
-            }
         }
         return false;
     }
-
 }
