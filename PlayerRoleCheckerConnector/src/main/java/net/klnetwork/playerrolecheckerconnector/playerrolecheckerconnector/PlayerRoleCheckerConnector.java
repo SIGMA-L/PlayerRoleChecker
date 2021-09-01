cd4181ae49cf552673b007516ae9e9e3740f5857
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-09-01 18:43:33 +0900

    added SQLUtil

--- PlayerRoleCheckerConnector/src/main/java/net/klnetwork/playerrolecheckerconnector/playerrolecheckerconnector/PlayerRoleCheckerConnector.java
+++ PlayerRoleCheckerConnector/src/main/java/net/klnetwork/playerrolecheckerconnector/playerrolecheckerconnector/PlayerRoleCheckerConnector.java
@@ -1,11 +1,20 @@
 package net.klnetwork.playerrolecheckerconnector.playerrolecheckerconnector;
 
+import net.klnetwork.playerrolecheckerconnector.playerrolecheckerconnector.Util.SQLUtil;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PlayerRoleCheckerConnector extends JavaPlugin {
 
+
+    public static Plugin plugin;
+
+
     @Override
     public void onEnable() {
+        plugin = this;
+        saveDefaultConfig();
+        SQL.init();
         // Plugin startup logic
 
     }
---
+++ PlayerRoleCheckerConnector/src/main/java/net/klnetwork/playerrolecheckerconnector/playerrolecheckerconnector/SQL.java
@@ -0,0 +1,47 @@
+package net.klnetwork.playerrolecheckerconnector.playerrolecheckerconnector;
+
+
+import net.klnetwork.playerrolecheckerconnector.playerrolecheckerconnector.Util.SQLUtil;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+import static net.klnetwork.playerrolecheckerconnector.playerrolecheckerconnector.PlayerRoleCheckerConnector.*;
+import static org.bukkit.Bukkit.getServer;
+
+public class SQL {
+
+    public static String SQLLocate;
+    public static String Server;
+    public static Integer Port;
+    public static String DB;
+    public static String UserName;
+    public static String PassWord;
+    public static String Option;
+
+    public static void init() {
+        try {
+            SQLLocate = plugin.getConfig().getString("SQLite.SQLiteLocate");
+            Server = plugin.getConfig().getString("MySQL.Server");
+            Port = plugin.getConfig().getInt("MySQL.Port");
+            DB = plugin.getConfig().getString("MySQL.Database");
+            UserName = plugin.getConfig().getString("MySQL.Username");
+            PassWord = plugin.getConfig().getString("MySQL.Password");
+            Option = plugin.getConfig().getString("MySQL.Option");
+        } catch (Exception e) {
+            getServer().getLogger().info("getConfigError");
+        }
+        try {
+            Statement SQLSt = SQLUtil.getSQLConnection().createStatement();
+            SQLSt.setQueryTimeout(30);
+            SQLSt.executeUpdate("create table if not exists verifyplayer (uuid VARCHAR(50),discord VARCHAR(50))");
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+}
+
