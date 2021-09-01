95a0ae0dbd453464d9e425e54c9d180b9c2043fa
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-09-01 17:59:10 +0900

    TEST BUILD

---
+++ PlayerRoleCheckerConnector/src/main/java/net/klnetwork/playerrolecheckerconnector/playerrolecheckerconnector/PlayerRoleCheckerConnector.java
@@ -0,0 +1,17 @@
+package net.klnetwork.playerrolecheckerconnector.playerrolecheckerconnector;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlayerRoleCheckerConnector extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
