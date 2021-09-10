5c0fc1d09ac27c871802f27f510776cb56893239
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2021-09-10 16:58:11 +0900

    ConnectionIsDead Check

--- PlayerRoleChecker/src/main/java/net/klnetwork/playerrolechecker/Util/SQLUtil.java
+++ PlayerRoleChecker/src/main/java/net/klnetwork/playerrolechecker/Util/SQLUtil.java
@@ -4,11 +4,11 @@ package net.klnetwork.playerrolechecker.Util;
 import java.sql.*;
 
 import static net.klnetwork.playerrolechecker.PlayerRoleChecker.plugin;
-import static net.klnetwork.playerrolechecker.SQL.*;
 
 public class SQLUtil {
 
     private static Connection connection;
+    private static long connectionAlive = 0;
 
     public static String[] getDiscordFromSQL(String uuid) {
         String[] result = null;
@@ -82,15 +82,29 @@ public class SQLUtil {
     }
 
     public static Connection getSQLConnection() throws SQLException {
-        if (connection == null || connection.isClosed()) {
+        if (connection == null || connection.isClosed() || !ConnectionIsDead()) {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
             connection = DriverManager.getConnection("jdbc:mysql://" + plugin.getConfig().getString("MySQL.Server") + ":" + plugin.getConfig().getInt("MySQL.Port") + "/" + plugin.getConfig().getString("MySQL.Database") + plugin.getConfig().getString("MySQL.Option"), plugin.getConfig().getString("MySQL.Username"), plugin.getConfig().getString("MySQL.Password"));
+            System.out.println("[DEBUG] Connection is Regenerated!");
         }
         return connection;
+    }
+
+
+    /*
+     * https://github.com/Elikill58/Negativity/blob/0d17657f05b869ea191e3a7a95101c37b3af009b/src/com/elikill58/negativity/universal/Database.java#L47
+     */
+    public static boolean ConnectionIsDead() throws SQLException {
+        long nowTime = System.currentTimeMillis();
+        if (nowTime - connectionAlive > 900000) {
+            connectionAlive = nowTime;
+            return connection.isValid(1);
+        }
+        return true;
     }
 }
 
--- PlayerRoleCheckerConnector/src/main/java/net/klnetwork/playerrolecheckerconnector/Util/SQLUtil.java
+++ PlayerRoleCheckerConnector/src/main/java/net/klnetwork/playerrolecheckerconnector/Util/SQLUtil.java
@@ -4,12 +4,12 @@ package net.klnetwork.playerrolecheckerconnector.Util;
 import java.sql.*;
 
 import static net.klnetwork.playerrolecheckerconnector.PlayerRoleCheckerConnector.plugin;
-import static net.klnetwork.playerrolecheckerconnector.SQL.*;
 
 
 public class SQLUtil {
 
     private static Connection connection;
+    private static long connectionAlive = 0;
 
     public static String[] getDiscordFromSQL(String uuid) {
         String[] result = null;
@@ -80,15 +80,29 @@ public class SQLUtil {
     }
 
     public static Connection getSQLConnection() throws SQLException {
-        if (connection == null || connection.isClosed()) {
+        if (connection == null || connection.isClosed() || !ConnectionIsDead()) {
             try {
                 Class.forName("com.mysql.jdbc.Driver");
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
             }
             connection = DriverManager.getConnection("jdbc:mysql://" + plugin.getConfig().getString("MySQL.Server") + ":" + plugin.getConfig().getInt("MySQL.Port") + "/" + plugin.getConfig().getString("MySQL.Database") + plugin.getConfig().getString("MySQL.Option"), plugin.getConfig().getString("MySQL.Username"), plugin.getConfig().getString("MySQL.Password"));
+            System.out.println("[DEBUG] Connection is Regenerated!");
         }
         return connection;
+    }
+
+
+    /*
+     * https://github.com/Elikill58/Negativity/blob/0d17657f05b869ea191e3a7a95101c37b3af009b/src/com/elikill58/negativity/universal/Database.java#L47
+     */
+    public static boolean ConnectionIsDead() throws SQLException {
+        long nowTime = System.currentTimeMillis();
+        if (nowTime - connectionAlive > 900000) {
+            connectionAlive = nowTime;
+            return connection.isValid(1);
+        }
+        return true;
     }
 }
 
