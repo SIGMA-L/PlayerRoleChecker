fdf6ca9922221880eadcc129ff13f08da8db8683
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2022-07-11 00:28:14 +0900

    連携システム実装

--- PlayerRoleCheckerAPI/src/main/java/net/klnetwork/playerrolechecker/api/PlayerRoleCheckerAPI.java
+++ PlayerRoleCheckerAPI/src/main/java/net/klnetwork/playerrolechecker/api/PlayerRoleCheckerAPI.java
@@ -1,10 +1,14 @@
 package net.klnetwork.playerrolechecker.api;
 
+import net.dv8tion.jda.internal.utils.tuple.Pair;
+import net.klnetwork.playerrolechecker.api.data.APIHook;
 import net.klnetwork.playerrolechecker.api.enums.HookedAPIType;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
+
+import java.util.*;
 
 public class PlayerRoleCheckerAPI {
-
     public static boolean isConnector() {
         final HookedAPIType type = getHookedAPIType();
 
@@ -18,6 +22,39 @@ public class PlayerRoleCheckerAPI {
     }
 
     public static HookedAPIType getHookedAPIType() {
-        return HookedAPIType.NONE;
+        if (pairs.isEmpty()) {
+            init();
+        }
+
+        return getAPIType();
+    }
+
+    private static HookedAPIType getAPIType() {
+        List<HookedAPIType> requireType = new ArrayList<>(Arrays.asList(HookedAPIType.CHECKER, HookedAPIType.CONNECTOR));
+        pairs.keySet().forEach(requireType::remove);
+
+        if (requireType.size() == 0) {
+            return HookedAPIType.BOTH;
+        } else if (requireType.size() == 1) {
+            return requireType.get(0);
+        } else {
+            return HookedAPIType.NONE;
+        }
+    }
+
+    private static final Map<HookedAPIType, APIHook> pairs = new HashMap<>();
+
+    public static void init() {
+        pairs.clear();
+
+        List<Plugin> plugins = Arrays.asList(Bukkit.getPluginManager().getPlugins());
+
+        plugins.forEach(plugin -> {
+            if (plugin instanceof APIHook) {
+                APIHook hook = (APIHook) plugin;
+
+                pairs.put(hook.getType(), hook);
+            }
+        });
     }
 }
