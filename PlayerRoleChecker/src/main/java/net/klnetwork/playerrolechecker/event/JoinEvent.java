eb8c63bb73969f8d0c70eeda824b9099a5cb0b51
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2022-07-29 07:36:57 +0900

    Added dependency

--- PlayerRoleChecker/src/main/java/net/klnetwork/playerrolechecker/event/JoinEvent.java
+++ PlayerRoleChecker/src/main/java/net/klnetwork/playerrolechecker/event/JoinEvent.java
@@ -2,8 +2,8 @@ package net.klnetwork.playerrolechecker.event;
 
 import net.klnetwork.playerrolechecker.PlayerRoleChecker;
 import net.klnetwork.playerrolechecker.api.CodeUtil;
-import net.klnetwork.playerrolechecker.util.OtherUtil;
 import net.klnetwork.playerrolechecker.table.LocalSQL;
+import net.klnetwork.playerrolechecker.util.OtherUtil;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
