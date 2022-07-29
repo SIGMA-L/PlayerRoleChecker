9cb7046ba3d5d315d6effa25cf61183408b1ab05
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2022-07-30 08:53:20 +0900

    remove length-big message

--- PlayerRoleCheckerAPI/src/main/java/net/klnetwork/playerrolechecker/api/data/JoinHandler.java
+++ PlayerRoleCheckerAPI/src/main/java/net/klnetwork/playerrolechecker/api/data/JoinHandler.java
@@ -1,10 +1,14 @@
 package net.klnetwork.playerrolechecker.api.data;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 
-public abstract class JoinHandler {
-    public void onPreLoginEvent(AsyncPlayerPreLoginEvent event) {};
+public abstract class JoinHandler implements Listener {
+    @EventHandler
+    public void onPreLoginEvent(AsyncPlayerPreLoginEvent event) {}
 
-    public void onLoginEvent(PlayerLoginEvent event) {};
+    @EventHandler
+    public void onLoginEvent(PlayerLoginEvent event) {}
 }
