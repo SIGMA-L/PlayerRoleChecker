a794560cc006b6aabdd707d406be31be79e4b357
SIGMA-L <67269568+SIGMA-L@users.noreply.github.com>
2022-08-01 17:12:28 +0900

    Create index.js

---
+++ docs/index.js
@@ -0,0 +1,2 @@
+var lang = window.navigator.languages || window.navigator.language || window.navigator.userLanguage || window.navigator.browserLanguage;
+if(lang == 'ja'){ location.href = 'https://google.com'; }
