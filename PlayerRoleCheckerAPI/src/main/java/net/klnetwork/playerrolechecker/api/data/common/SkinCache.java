828509260f8e1ee864c5f4f4284d78ee0ca9aa97
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2022-09-05 17:30:07 +0900

    geyserがtextureIdを返さない場合steveの画像を出す

--- PlayerRoleCheckerAPI/src/main/java/net/klnetwork/playerrolechecker/api/data/common/SkinCache.java
+++ PlayerRoleCheckerAPI/src/main/java/net/klnetwork/playerrolechecker/api/data/common/SkinCache.java
@@ -2,19 +2,38 @@ package net.klnetwork.playerrolechecker.api.data.common;
 
 import net.klnetwork.playerrolechecker.api.utils.CommonUtils;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 import java.util.UUID;
 
 public class SkinCache {
+    private static BufferedImage steve;
+
+    public BufferedImage getSteveImage() {
+        try {
+            steve = ImageIO.read(Objects.requireNonNull(SkinCache.class.getResourceAsStream("/steve.png")));
+        } catch (IOException e) {
+            /* ignored */
+        }
+
+        return steve;
+    }
+
     private BufferedImage skin;
 
     public BufferedImage getSkin(UUID uuid, boolean f) {
-        if (uuid == null) {
-            return null;
-        }
+        try {
+            if (uuid == null) {
+                return getSteveImage();
+            }
 
-        if (skin == null) {
-            skin = CommonUtils.getHeadSkin(uuid, f);
+            if (skin == null) {
+                skin = CommonUtils.getHeadSkin(uuid, f);
+            }
+        } catch (Exception ex) {
+            skin = getSteveImage();
         }
 
         return skin;
