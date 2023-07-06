2872bc8ff2e481b4e95e661506155568ebfa14cb
KoutaChan <76502083+KoutaChan@users.noreply.github.com>
2023-07-07 05:10:11 +0900

    Fix Compile Error

--- PlayerRoleChecker/src/main/java/net/klnetwork/playerrolechecker/api/CustomDataBaseImpl.java
+++ PlayerRoleChecker/src/main/java/net/klnetwork/playerrolechecker/api/CustomDataBaseImpl.java
@@ -2,18 +2,11 @@ package net.klnetwork.playerrolechecker.api;
 
 import net.klnetwork.playerrolechecker.api.data.PlayerDataTable;
 import net.klnetwork.playerrolechecker.api.data.checker.CheckerCustomDataBase;
-import net.klnetwork.playerrolechecker.api.data.common.TemporaryTable;
-import net.klnetwork.playerrolechecker.table.LocalSQL;
 import net.klnetwork.playerrolechecker.table.PlayerDataSQL;
 
 public class CustomDataBaseImpl implements CheckerCustomDataBase {
     @Override
     public void setPlayerData(PlayerDataTable table) {
         PlayerDataSQL.setInstance(table);
-    }
-
-    @Override
-    public void setTemporary(TemporaryTable table) {
-        LocalSQL.setInstance(table);
     }
 }
